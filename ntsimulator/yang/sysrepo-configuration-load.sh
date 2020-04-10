a19f893a70c03dee9312e1208301d079409d84ac
Alex Stancu <alexandru.stancu@highstreet-technologies.com>
2020-04-10 18:19:53 +0300

    Add counters for generated notifications.
    Add status counters for generated notifications (VES and NETCONF).
    
    Issue-ID: SIM-21
    Change-Id: I370532efd7bb9d06f3ed81a2f66cdef4e9750a51
    Signed-off-by: Alex Stancu <alexandru.stancu@highstreet-technologies.com>

--- ntsimulator/yang/sysrepo-configuration-load.sh
+++ ntsimulator/yang/sysrepo-configuration-load.sh
@@ -20,16 +20,6 @@ sleep 20
 
 echo "Loading data into sysrepo..."
 
-#SSH_PUB_KEY="$(cat /home/netconf/.ssh/id_dsa.pub| awk '{print $2}')"
-
-#echo '<system xmlns="urn:ietf:params:xml:ns:yang:ietf-system"><authentication><user><name>netconf</name><authorized-key><name>ssh_key</name><algorithm>ssh-dss</algorithm>' >> load_auth_pubkey.xml
-#echo '<key-data>'"$SSH_PUB_KEY"'</key-data></authorized-key></user></authentication></system>' >> load_auth_pubkey.xml
-
-#sysrepocfg --merge=load_auth_pubkey.xml --format=xml ietf-system
-#rm load_auth_pubkey.xml
-#
-#ssh-keyscan -p 830 localhost >> ~/.ssh/known_hosts
-
 pyang -f sample-xml-skeleton --sample-xml-list-entries 2 *.yang
 
 result=$(netopeer2-cli <<-END
