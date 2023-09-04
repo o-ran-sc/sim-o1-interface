a4bf07b78471b645cd098cd5b51c6c4382470318
ktimoney <kevin.timoney@est.tech>
2023-09-04 14:47:52 +0100

    Update readthedocs config
    Issue-ID: NONRTRIC-921
    Change-Id: Ib140defde32e18c3dfb02a1c165702efb4773c8f
    Signed-off-by: ktimoney <kevin.timoney@est.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,36 @@
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2021-2022 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
 from docs_conf.conf import *
+
+#branch configuration
+
+branch = 'latest'
+
 linkcheck_ignore = [
     'http://localhost.*',
     'http://127.0.0.1.*',
     'https://gerrit.o-ran-sc.org.*',
     'http://sdn-controller-hostname*'
 ]
+
+extensions = ['sphinxcontrib.redoc', 'sphinx.ext.intersphinx',]
+
+#intershpinx mapping with other projects
+intersphinx_mapping = {}
+
+intersphinx_mapping['nonrtric'] = ('https://docs.o-ran-sc.org/projects/o-ran-sc-nonrtric/en/%s' % branch, None)
